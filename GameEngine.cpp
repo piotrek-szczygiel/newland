5322de0390f855f02c59477d28af6dc480c1f032
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-06-23 14:40:55 +0200

    Initial commit

---
+++ GameEngine.cpp
@@ -0,0 +1,351 @@
+#include "GameEngine.hpp"
+#include "Chronometer.hpp"
+#include "Controls.hpp"
+#include <Windows.h>
+#include <string.h>
+
+sf::RenderWindow *GameEngine::renderWindow;
+sf::Event GameEngine::event;
+sf::Text GameEngine::versionText;
+
+sf::Vector2f GameEngine::screenDimensions;
+bool GameEngine::fullscreen;
+std::string GameEngine::loadMap;
+
+bool GameEngine::mainMenu;
+
+sf::Font GameEngine::font;
+
+bool GameEngine::showFps;
+bool GameEngine::showVersion;
+
+sftools::Chronometer GameEngine::runtimeClock;
+
+bool GameEngine::paused=false;
+
+void GameEngine::PauseRuntime()
+{
+	paused=true;
+	runtimeClock.pause();
+	Game::PauseNpcs(true);
+}
+
+void GameEngine::ResumeRuntime()
+{
+	paused=false;
+	runtimeClock.resume();
+	Game::PauseNpcs(false);
+}
+
+GameEngine::~GameEngine()
+{
+	delete renderWindow;
+}
+
+// Inicjalizacja silnika gry
+void GameEngine::Initialize(float x, float y, std::string title, bool _fullscreen, std::string arg)
+{
+	mainMenu = true;
+
+	// Ustawienie rozdzielszosci okna
+	screenDimensions.x = x;
+	screenDimensions.y = y;
+
+	// Wczytana mapa bedzie ta podana przez argument
+	loadMap = arg;
+
+	std::string name="Gracz";
+	//std::cout << "Wprowadz imie dla gracza: ";
+	//std::cin >> name;
+
+	// Utworzenie okna SFML na podstawie przekazanych parametrow
+	renderWindow = new sf::RenderWindow(sf::VideoMode((unsigned int)screenDimensions.x,(unsigned int)screenDimensions.y), title, _fullscreen ? sf::Style::Fullscreen : sf::Style::Close);
+	//renderWindow->setVerticalSyncEnabled(true);
+	// Utworzenie obiektu walsciwej gry
+	if(_fullscreen)renderWindow->setMouseCursorVisible(false);
+
+	fullscreen = _fullscreen;
+
+	Game::Initialize(renderWindow, name);
+
+	// Wczytanie ikony okna
+	sf::Image icon;
+    if (icon.loadFromFile("Resources/ui/icon.png")) {
+		renderWindow->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    }
+
+	versionText.setString(title);
+
+	showFps = true;
+	showVersion = false;
+}
+
+// Procedura uruchamiajaca wlasciwa gre
+int GameEngine::Run()
+{
+	if(sf::Joystick::isConnected(0))
+	{
+		std::cout << "Wykryto kontroler!\nSterowanie padem!" << std::endl;
+		Controls::device = Controls::DeviceJoystick;
+	}
+	else
+	{
+		std::cout << "\nNie wykryto kontrolera!\nSterowanie klawiatura!" << std::endl;
+		Controls::device = Controls::DeviceKeyboard;
+	}
+
+	font.loadFromFile("Resources/tahoma.ttf");
+
+	sf::Text fpsText;
+	fpsText.setFont(font);
+	fpsText.setCharacterSize(18);
+	fpsText.setColor(sf::Color(0,255,0,128));
+
+	versionText.setFont(font);
+	versionText.setCharacterSize(18);
+	versionText.setColor(sf::Color(255,255,255,128));
+
+	// Losowanie seeda
+	srand (time(NULL));
+
+	int returnCode = 0;
+	int frames = 0;
+
+	// Glowne zegary
+	 // Calkowity czas dzialania gry
+	sftools::Chronometer frametimeClock; // Czas renderowania pojedynczej klatki
+	sf::Clock fpsClock; // Zegar umozliwiajacy obliczenie FPS-ow
+
+	runtimeClock.reset();
+
+	// Petla wykonujaca sie przez czas otwarcia okna gry
+	while(renderWindow -> isOpen() && Game::running)
+	{
+		if(mainMenu)
+		{
+			MainMenu();
+			mainMenu=false;
+			continue;
+		}
+		// Pobranie eventow okna
+		while(renderWindow -> pollEvent(event))
+		{
+			// Jezeli zamknieto okno, wyjdz z gry
+			if(event.type == sf::Event::Closed) {
+				renderWindow -> close();
+				returnCode = 1;
+				continue;
+			}
+		}
+
+		if(Controls::IsPressed(Controls::Pause))
+			PauseRuntime();
+		else if(Controls::IsPressed(Controls::Unpause))
+			ResumeRuntime();
+		else if(sf::Keyboard::isKeyPressed(sf::Keyboard::F12))
+			renderWindow -> close();
+
+		// Czas pojedynczej klatki
+		sf::Time frameTime;
+		// Jezeli jestesmy w GUI gry (w trakcie grania)
+
+		if(!paused)
+		{
+			// Wznawiamy zegary jezeli byly zatrzymane
+			frametimeClock.resume();
+			runtimeClock.resume();
+
+			// Obliczenie czasu klatki i zresetowanie zegara
+			frameTime = frametimeClock.reset(true);
+			sf::Time tpf = sf::seconds(1.f / 10.f);
+
+			 // W przypadku dluzszego zaciecia gry, eliminuje przekazanie zbyt duzej wartosci czasu klatki
+			// co moglo by spowodowac np. przejscie przez mur itp.
+			if(frameTime > tpf)
+				frameTime = tpf; 
+
+			Game::PauseNpcs(false);
+           
+			// Aktualizujemy stan gry
+			Game::Update(frameTime);
+
+			// Rysujemy klatke
+			Game::Draw(*renderWindow, sf::RenderStates::Default);
+
+			// Jezeli minela sekunda
+			if(showFps && fpsClock.getElapsedTime().asSeconds()>=1.f)
+			{
+				// To obliczamy ilosc klatek ktory wygenerowalismy oraz zerujemy zegar je zliczajacy
+				float fpsTime=fpsClock.restart().asSeconds();
+				float fps = (float)frames / fpsTime;
+				fps = floor(fps + 0.5);
+			
+				std::string stringFps = "FPS: " + std::to_string((int)fps);
+				fpsText.setString(stringFps);
+				// Zerujemy zmienna zliczajaca klatki
+				frames=0;
+			}
+
+			fpsText.setPosition(Camera::cameraPosition.x+screenDimensions.x-fpsText.getGlobalBounds().width-5, Camera::cameraPosition.y);
+			versionText.setPosition(Camera::cameraPosition);
+
+			if(showFps)
+				renderWindow->draw(fpsText);
+			if(showVersion)
+				renderWindow->draw(versionText);
+
+			// Wyswietlamy klatke z BackBuffera
+			renderWindow -> display();
+
+			// Dodajemy zmienna zliczajaca klatki
+			frames++;
+		}
+	}
+	
+
+	// Jezeli wyszlismy inaczej niz zamkajac okno, sprawdzamy czy jest dalej otwarte
+	if(renderWindow->isOpen())
+		// Jezeli jest, to zamykamy je
+		renderWindow->close();
+	
+	Maps::DeleteLocations();
+	// Zwracamy kod-zwrotny
+	return returnCode;
+}
+
+void GameEngine::MainMenu()
+{
+	sf::Clock timer;
+
+	int position = 0;
+	const int max_position = 1;
+
+	std::vector<sf::Texture*> menuTextures;
+	std::vector<sf::Sprite> menuSprites;
+
+	int actualFrame = 0;
+	int maxFrames = 59;
+
+	for(int i = 0; i < maxFrames; i++)
+	{
+		sf::Texture* frame = new sf::Texture;
+		std::string file = "Resources/menuanimation/";
+		file += std::to_string(i+1);
+		file += ".png";
+		frame->loadFromFile(file);
+
+		sf::Sprite frameSprite(*frame);
+		menuSprites.push_back(frameSprite);
+		menuTextures.push_back(frame);
+	}
+
+	sf::Texture logoTexture;
+	logoTexture.loadFromFile("Resources/ui/logo.png");
+	sf::Sprite logo(logoTexture);
+	logo.setPosition(GameEngine::screenDimensions.x / 2 - (logo.getGlobalBounds().width /2 ), 50);
+
+	sf::Music muzyka;
+	muzyka.openFromFile("Resources/music/menu.ogg");
+	muzyka.setLoop(true);
+	muzyka.play();
+	muzyka.setVolume(25);
+	
+	sf::Texture nowagraTexture, nowagraActiveTexture;
+	nowagraTexture.loadFromFile("Resources/ui/nowagra.png");
+	nowagraActiveTexture.loadFromFile("Resources/ui/nowagra_active.png");
+	sf::Sprite nowagra(nowagraTexture);
+	sf::Sprite nowagraActive(nowagraActiveTexture);
+
+	sf::Texture wyjscieTexture, wyjscieActiveTexture;
+	wyjscieTexture.loadFromFile("Resources/ui/wyjscie.png");
+	wyjscieActiveTexture.loadFromFile("Resources/ui/wyjscie_active.png");
+	sf::Sprite wyjscie(wyjscieTexture);
+	sf::Sprite wyjscieActive(wyjscieActiveTexture);
+	
+	sf::Sprite* pozycje[max_position+1]={&nowagra, &wyjscie};
+	sf::Sprite* pozycje_active[max_position+1]={&nowagraActive,&wyjscieActive};
+
+
+	sf::Clock frameTimer;
+	sf::Time lastFrame;
+	sf::Time frameInverval = sf::seconds(0.05f);
+
+	while(renderWindow->isOpen())
+	{
+		while(renderWindow -> pollEvent(event))
+		{
+			// Jezeli zamknieto okno, wyjdz z gry
+			if(event.type == sf::Event::Closed) {
+				renderWindow -> close();
+				return;
+			}
+			else if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+			{
+				renderWindow -> close();
+				return;
+			}
+		}
+
+		if (Controls::IsPressed(Controls::Down))
+		{
+			if(position+1 <= max_position)
+			{
+				++position;
+			}
+		}
+		else if (Controls::IsPressed(Controls::Up))
+		{
+			if(position-1 >= 0)
+			{
+				--position;
+			}
+		}
+
+		if(Controls::IsPressed(Controls::Accept))
+		{
+			for(int i = 0; i < 59; i++)
+			{
+				delete menuTextures[i];
+			}
+
+			switch(position)
+			{
+			case 0:
+				Game::LoadMap(loadMap);
+				return;
+				break;
+			case 1:
+				renderWindow->close();
+				return;
+				break;
+			}
+		}
+
+		renderWindow->clear();
+
+		renderWindow->draw(menuSprites[actualFrame]);
+		
+		renderWindow->draw(logo);
+
+		if(lastFrame + frameInverval <= frameTimer.getElapsedTime())
+			actualFrame++, lastFrame=frameTimer.getElapsedTime();
+
+		if(actualFrame + 1 == maxFrames) actualFrame = 0;
+
+		for(int i = 0; i <= max_position; i++)
+		{
+			if(i == position)
+			{
+				pozycje_active[i]->setPosition(GameEngine::screenDimensions.x / 2 - (pozycje_active[i]->getGlobalBounds().width / 2), 240 + i * 60);
+				renderWindow->draw(*pozycje_active[i]);
+			}
+			else
+			{
+				pozycje[i]->setPosition(GameEngine::screenDimensions.x / 2 - (pozycje_active[i]->getGlobalBounds().width / 2), 240 + i * 60);
+				renderWindow->draw(*pozycje[i]);
+			}
+		}
+
+		renderWindow->display();
+	}
+}
---
+++ Location.cpp
@@ -0,0 +1,609 @@
+#include "Location.hpp"
+#include "Player.hpp"
+#include "Game.hpp"
+const double M_PI = 3.14159265358979323846;
+// Konstruktor obiektu lokacji
+Location::Location()
+{
+	npcs = new std::vector<Npc*>();
+	enemies = new std::vector<Enemy*>();
+	
+	blendStates.blendMode = sf::BlendMultiply;
+	blendStates2.blendMode = sf::BlendAdd;
+	
+	noDaylightColor = sf::Color::Color(128,128,128);
+	
+	daynightColors.push_back(DayNightColorsInfo(sf::Color::Color(230, 230, 215),false));
+	daynightColors.push_back(DayNightColorsInfo(sf::Color::Color(196, 196, 192),false));
+	daynightColors.push_back(DayNightColorsInfo(sf::Color::Color(162, 162, 169),false));
+	daynightColors.push_back(DayNightColorsInfo(sf::Color::Color(128, 128, 146),false));
+	daynightColors.push_back(DayNightColorsInfo(sf::Color::Color(94, 94, 123),true));
+	daynightColors.push_back(DayNightColorsInfo(sf::Color::Color(60,60,100),true));
+}
+
+Location::~Location()
+{
+	for(int i = 0; i < npcs->size(); i++)
+	{
+		delete npcs->at(i);
+	}
+
+	for(int i = 0; i < enemies->size(); i++)
+	{
+		delete enemies->at(i);
+	}
+
+	for(int i = 0; i < pickups.size(); i++)
+	{
+		delete pickups[i];
+	}
+
+	for(int i = 0; i < textures.size(); i++)
+	{
+		delete textures[i].texture;
+	}
+
+	for(int i = 0; i < animations.size(); i++)
+	{
+		delete animations[i].animatedSprite;
+		delete animations[i].animation;
+	}
+
+	delete npcs, enemies;
+
+}
+
+std::vector<tmx::MapObject>* Location::GetTriggers()
+{
+	for(int i = 0; i < objectLayers.size(); i++)
+	{
+		if(objectLayers[i].layerName == "Trigger")
+		{
+			return objectLayers[i].objects;
+		}
+	}
+	
+	return NULL;
+}
+
+std::vector<tmx::MapObject>* Location::GetObjects()
+{
+	for(int i = 0; i < objectLayers.size(); i++)
+	{
+		if(objectLayers[i].layerName == "Object")
+		{
+			return objectLayers[i].objects;
+		}
+	}
+	
+	return NULL;
+}
+
+std::vector<Npc*>* Location::GetNpcs()
+{
+	return npcs;
+}
+
+Npc* Location::GetNpc(std::wstring name)
+{
+	for(int i = 0; i < npcs->size(); i++)
+	{
+		if(npcs->at(i)->GetName()==name)
+			return npcs->at(i);
+	}
+
+	return NULL;
+}
+
+std::string Location::GetBgMusic()
+{
+	return bgMusic;
+}
+
+void Location::SetLayerVisible(std::string layerName, bool visible)
+{
+	for(auto layer = mapLoader -> GetLayers().begin(); layer != mapLoader -> GetLayers().end(); ++layer)
+	{
+		if(layer->name == layerName)
+			layer->visible = visible;
+	}
+}
+
+// Procedura wczytujaca mape
+void Location::Load(std::string fileName)
+{
+	// Wczytanie mapy z danego pliku
+	mapLoader = new tmx::MapLoader("Resources/maps/");
+	mapLoader->AddSearchPath("Resources/tilesets/");
+	mapLoader -> Load(fileName + ".tmx");
+
+	lightTex.loadFromFile("Resources/sprites/light.png");
+	lightTex.setSmooth(true);
+
+	lightBase.create(GetSize().x, GetSize().y);
+	lightBase.clear();
+
+	for(auto layer = mapLoader -> GetLayers().begin(); layer != mapLoader -> GetLayers().end(); ++layer)
+	{
+		// Dodanie obiektow mapy do tablicy objektow
+		if(layer->type == tmx::MapLayerType::ObjectGroup)
+		{
+			ObjectLayerInfo layerInfo;
+			layerInfo.layerName = layer->name;
+			layerInfo.objects = new std::vector<tmx::MapObject>;
+			for(auto& obj : layer->objects)
+				layerInfo.objects->push_back(obj);
+
+			objectLayers.push_back(layerInfo);
+		}
+	}
+
+	// Wczytanie nazwy mapy
+	locationName = mapLoader->GetPropertyString("level") == "" ? fileName : mapLoader->GetPropertyString("level");
+	parentLocation = mapLoader->GetPropertyString("parent") == "" ? fileName : mapLoader->GetPropertyString("parent");
+	daylight = mapLoader->GetPropertyString("daylight") == "true" ? true : false;
+
+
+	// Wczytanie nazwy pliku myzki w tle
+	bgMusic      = "Resources/music/" + mapLoader->GetPropertyString("music");
+
+	for(auto layer = objectLayers.begin(); layer != objectLayers.end(); ++layer)
+	{
+		if(layer->layerName == "Npc")
+		{
+			// Dodawanie npc
+			for(auto obj = layer->objects->begin(); obj != layer->objects->end(); ++obj)
+			{
+				Npc *npc;
+				npc = new Npc();
+				if(!npc->Load(obj->GetPropertyString("file")))
+				{
+					delete npc;
+					continue;
+				}
+				npc->player.SetPosition(obj->GetPosition());
+				npc->player.Move(sf::Vector2f(0,0));
+
+				if(!npc->free)
+				{
+					bool found = false;
+
+					for(int i = 0; i < objectLayers.size(); i++)
+					{
+						if(objectLayers[i].layerName == "NpcPaths")
+						{
+							for(int j = 0; j < objectLayers[i].objects->size(); j++)
+							{
+								if(obj->GetName() == objectLayers[i].objects->at(j).GetName())
+								{
+									npc->SetPaths(objectLayers[i].objects->at(j).GetAABB());
+									found = true;
+								}
+							}
+							break;
+						}
+					}
+
+					if(!found)
+						npc->staticNpc = true;
+					else
+						npc->staticNpc = false;
+				}
+
+				npcs->push_back(npc);
+			}
+		}
+		else if(layer->layerName == "Enemies")
+		{
+			// Dodawanie npc
+			for(auto obj = layer->objects->begin(); obj != layer->objects->end(); ++obj)
+			{
+				for(int i = 0; i < 4; i++)
+				{
+					Enemy *enemy;
+					enemy = new Enemy();
+					if(!enemy->Load(obj->GetPropertyString("file")))
+					{
+						delete enemy;
+						continue;
+					}
+					switch(i)
+					{
+					case 0:
+						enemy->SetPosition(sf::Vector2f(obj->GetAABB().left + 40, obj->GetAABB().top + 40));
+						break;
+					case 1:
+						enemy->SetPosition(sf::Vector2f(obj->GetAABB().left + 40, obj->GetAABB().top + (obj->GetAABB().height) - 40));
+						break;
+					case 2:
+						enemy->SetPosition(sf::Vector2f(obj->GetAABB().left + (obj->GetAABB().width) - 40, obj->GetAABB().top + 40));
+						break;
+					case 3:
+						enemy->SetPosition(sf::Vector2f(obj->GetAABB().left + (obj->GetAABB().width) - 40, obj->GetAABB().top + (obj->GetAABB().height) - 40));
+						break;
+					case 4:
+						enemy->SetPosition(sf::Vector2f(obj->GetAABB().left + (obj->GetAABB().width/2), obj->GetAABB().top + (obj->GetAABB().height/2)));
+						break;
+					default:
+						break;
+					}
+					enemy->Move(sf::Vector2f(0,0));
+					enemy->SetPaths(obj->GetAABB());
+					enemies->push_back(enemy);
+				}
+			}
+
+		}
+		else if(layer->layerName == "Light")
+		{
+			for(auto obj = layer->objects->begin(); obj != layer->objects->end(); ++obj)
+			{
+				sf::Sprite light(lightTex);
+				float scalex, scaley;
+				scalex = scaley = obj->GetPropertyString("scale") == "" ? 1.f : std::atof(obj->GetPropertyString("scale").c_str());
+				light.setScale(scalex, scaley);
+				light.setPosition(obj->GetCentre().x-(light.getGlobalBounds().width/2), obj->GetCentre().y-(light.getGlobalBounds().height/2));
+				
+				std::string lightColor = obj->GetPropertyString("color") == "" ? "white" : obj->GetPropertyString("color");
+
+				if(lightColor == "yellow") light.setColor(sf::Color(255,255,128));
+				else if(lightColor == "orange") light.setColor(sf::Color(255,128,64));
+
+				lights.push_back(light);
+			}
+		}
+		else if(layer->layerName == "Pickup")
+		{
+			for(auto obj = layer->objects->begin(); obj != layer->objects->end(); ++obj)
+			{
+				Pickup *pickup = new Pickup(obj->GetName(), obj->GetPosition());
+				pickups.push_back(pickup);
+			}
+		}
+		else if(layer->layerName == "Animation")
+		{
+			// Dodawanie npc
+			for(auto obj = layer->objects->begin(); obj != layer->objects->end(); ++obj)
+			{
+				std::string file = "Resources/" + obj->GetPropertyString("file");
+				std::string spriteFile;
+				pugi::xml_document doc;
+				int frames=0;
+				float delay=0.2f;
+				int level=1;
+
+				if(doc.load_file(file.c_str()))
+				{
+					sf::Texture* spriteSheet = new sf::Texture;
+					pugi::xml_node panels = doc.child("animation");
+					for (pugi::xml_node panel = panels.first_child(); panel; panel = panel.next_sibling())
+					{
+						std::string name = panel.name();
+						std::string value = panel.child_value();
+
+						// Plik textury npc
+						if(name == "sprite")
+						{
+							std::string filepath = "Resources/sprites/"+value;
+							bool found = false;
+							
+							for(int i = 0; i < textures.size(); i++)
+							{
+								if(textures[i].filepath == filepath)
+								{
+									spriteSheet = textures[i].texture;
+									found = true;
+								}
+							}
+
+							if(!found)
+							{
+								spriteSheet->loadFromFile(filepath);
+
+								TextureInfo texInf;
+								texInf.filepath = filepath;
+								texInf.texture = spriteSheet;
+								textures.push_back(texInf);
+							}
+						}
+
+						// Nazwa npc
+						else if(name == "frames")
+						{
+							frames=std::atoi(value.c_str());
+						}
+
+						// Kierunek w ktorym npc sie bedzie patrzyl
+						else if(name == "delay")
+						{
+							delay=(float)std::atoi(value.c_str()) / 1000.f;
+						}
+
+						else if(name == "level")
+						{
+							level = std::atoi(value.c_str());
+						}
+					}
+
+					AnimationInfo animationInfo;
+					Animation* animation = new Animation;
+					AnimatedSprite* animatedSprite = new AnimatedSprite(sf::seconds(delay));
+
+					animation->setSpriteSheet(*spriteSheet);
+					for(int i = 0; i < frames; i++)
+						animation->addFrame(sf::IntRect(i*32, 0, 32, 32));
+
+					animatedSprite->setPosition(obj->GetPosition());
+					animatedSprite->play(*animation);
+
+					animationInfo.animatedSprite = animatedSprite;
+					animationInfo.animation = animation;
+
+					animationInfo.level = level;
+
+
+					animations.push_back(animationInfo);
+				}
+			}
+		}
+	}
+}
+
+
+sf::Vector2f Location::GetSpawnPoint(std::string portal)
+{
+	for(int i = 0; i < objectLayers.size(); i++)
+	{
+		if(objectLayers[i].layerName == "Portal")
+		{
+			for(int j = 0; j < objectLayers[i].objects->size(); j++)
+			{
+				if(objectLayers[i].objects->at(j).GetName()==portal)
+				{
+					return objectLayers[i].objects->at(j).GetPosition();
+				}
+			}
+		}
+	}
+	
+	return sf::Vector2f(0.f, 0.f);
+}
+
+void Location::PauseNpcs(bool pause)
+{
+	for(int i = 0; i < npcs->size(); i++)
+	{
+		npcs->at(i)->Pause(pause);
+	}
+}
+
+bool compareNpcs(Npc* npc1, Npc* npc2)
+{
+	return(npc1->player.GetPosition().y < npc2->player.GetPosition().y);
+}
+
+// Rysowanie mapy
+void Location::Draw(sf::RenderWindow &rt, sf::RenderStates states, Player* player, float runTime, float frameTime, int daynightType)
+{
+	std::sort(npcs->begin(), npcs->end(), compareNpcs);
+
+	for(int i = 0; i < npcs->size(); i++)
+	{
+		std::vector<sf::FloatRect> playerCollisions;
+
+		for(int j = 0; j < npcs->size(); j++)
+			if(j != i)
+				playerCollisions.push_back(npcs->at(j)->player.GetCollisionPoints());
+
+		playerCollisions.push_back(player->GetCollisionPoints());
+
+		npcs->at(i)->AiUpdate(frameTime, this, &playerCollisions);
+	}
+
+	for(int i = 0; i < enemies->size(); i++)
+	{
+		std::vector<sf::FloatRect> playerCollisions;
+
+		if(!enemies->at(i)->dead)
+		{
+			for(int j = 0; j < enemies->size(); j++)
+				if(j != i && !enemies->at(j)->dead)
+					playerCollisions.push_back(enemies->at(j)->GetCollisionPoints());
+
+			playerCollisions.push_back(player->GetCollisionPoints());
+		}
+		
+		enemies->at(i)->AiUpdate(frameTime, this, &playerCollisions, player->GetPosition());
+	}
+
+	// Rysowanie tylnich warst mapy (tych po ktorych gracz sie porusza)
+	int backLayers = 0;
+
+	for(int i = 0; i < animations.size(); i++)
+	{
+		if(animations[i].level == 0)
+		{
+			animations[i].animatedSprite->update(sf::seconds(frameTime));
+			rt.draw(*animations[i].animatedSprite, states);
+		}
+	}
+
+	for(auto layer = mapLoader -> GetLayers().begin(); layer != mapLoader -> GetLayers().end(); ++layer)
+	{
+		if(layer->name=="Cover")
+			break;
+
+		rt.draw(*layer, states);
+		backLayers++;
+	}
+
+	for(int i = 0; i < animations.size(); i++)
+	{
+		if(animations[i].level == 1)
+		{
+			animations[i].animatedSprite->update(sf::seconds(frameTime));
+			rt.draw(*animations[i].animatedSprite, states);
+		}
+	}
+
+	for(int i = 0; i < pickups.size(); i++)
+	{
+		pickups[i]->Draw(rt);
+	}
+
+	// Rysowanie npc-ow ktorzy sa za graczem
+	for(int i = 0; i < npcs->size(); i++)
+		if(npcs->at(i)->player.GetCollisionPoints().top < player->GetCollisionPoints().top)
+			npcs->at(i)->Draw(rt, states);
+
+	for(int i = 0; i < enemies->size(); i++)
+		if(!enemies->at(i)->dead && enemies->at(i)->GetCollisionPoints().top < player->GetCollisionPoints().top)
+			enemies->at(i)->DrawEnemy(rt, states);
+
+	// Rysowanie gracza
+	player->DrawPlayer(rt, states);
+
+	// Rysowanie npc-ow ktorzy sa przed graczem
+	for(int i = 0; i < npcs->size(); i++)
+		if(npcs->at(i)->player.GetCollisionPoints().top >= player->GetCollisionPoints().top)
+			npcs->at(i)->Draw(rt, states);
+
+	for(int i = 0; i < enemies->size(); i++)
+		if(!enemies->at(i)->dead && enemies->at(i)->GetCollisionPoints().top >= player->GetCollisionPoints().top)
+			enemies->at(i)->DrawEnemy(rt, states);
+
+	// Rysowanie przednich warstw mapy (tych za ktorymi gracz sie chowa)
+	for(auto layer = mapLoader -> GetLayers().begin()+backLayers; layer != mapLoader -> GetLayers().end(); ++layer)
+		rt.draw(*layer, states);
+
+	for(int i = 0; i < animations.size(); i++)
+	{
+		if(animations[i].level == 2)
+		{
+			animations[i].animatedSprite->update(sf::seconds(frameTime));
+			rt.draw(*animations[i].animatedSprite, states);
+		}
+	}
+
+	// Rysowanie nietoperzy
+	for(int i = 0; i < npcs->size(); i++)
+		if(npcs->at(i)->GetName() == L"#")
+			npcs->at(i)->Draw(rt, states);
+
+	for(int i = 0; i < npcs->size(); i++)
+		npcs->at(i)->player.DrawNick(rt, states, npcs->at(i)->interaction_message);
+
+	for(int i = 0; i < enemies->size(); i++)
+		enemies->at(i)->DrawNick(rt, states);
+
+	player->DrawNick(rt, states);
+
+	if(this->daynightType != daynightType)
+	{
+		this->daynightType = daynightType;
+		if(daylight)
+		{
+			lightBase.clear(daynightColors[daynightType].color);
+
+			if(daynightColors[daynightType].lights)
+				for(int i = 0; i < lights.size(); i++)
+					lightBase.draw(lights[i], blendStates2);
+		}
+		else
+		{
+			lightBase.clear(noDaylightColor);
+			for(int i = 0; i < lights.size(); i++)
+				lightBase.draw(lights[i], blendStates2);
+		}
+
+		lightBase.display();
+		lightBaseSpr.setTexture(lightBase.getTexture());
+	}
+
+	rt.draw(lightBaseSpr, blendStates);
+
+
+}
+
+void Location::AttackEnemy(sf::FloatRect playerWeaponHitbox)
+{
+	if((lastEnemyHit + sf::seconds(0.35f)) < timer.getElapsedTime())
+	{
+		for(int i = 0; i < enemies->size(); i++)
+		{
+			if(!enemies->at(i)->dead && enemies->at(i)->GetHitRange().intersects(playerWeaponHitbox))
+			{
+				std::cout << "Enemy hit!\n";
+				lastEnemyHit = timer.getElapsedTime();
+				enemies->at(i)->Damage(Game::player.damage);
+
+				if(enemies->at(i)->dead)
+				{
+					if((rand()%2)==1)
+					{
+						Pickup *pickup = new Pickup("czaszka", enemies->at(i)->GetPosition());
+						pickups.push_back(pickup);
+					}
+				}
+			}
+		}
+	}
+}
+
+sf::Vector2u Location::GetSize()
+{
+	return mapLoader -> GetMapSize();
+}
+
+bool Location::NpcCollision(sf::FloatRect playerCollisionRect)
+{
+	for(int i = 0; i <npcs->size(); i++)
+	{
+		if(npcs->at(i)->player.GetCollisionPoints().intersects(playerCollisionRect))
+			return true;
+	}
+
+	for(int i = 0; i <enemies->size(); i++)
+	{
+		if(!enemies->at(i)->dead && enemies->at(i)->GetCollisionPoints().intersects(playerCollisionRect))
+			return true;
+	}
+
+	return false;
+}
+
+// Sprawdzenie kolizji dla podanego obszaru
+bool Location::Collision(sf::FloatRect playerCollisionRect)
+{
+	bool collision = false;
+	for(int i = 0; i < objectLayers.size(); i++)
+	{
+		if(objectLayers[i].layerName=="Collision" || objectLayers[i].layerName=="Object")
+		{
+			for(int j = 0; j < objectLayers[i].objects->size(); j++)
+			{
+				if(objectLayers[i].objects->at(j).GetAABB().intersects(playerCollisionRect))
+					return true;
+			}
+		}
+	}
+
+	/*// Sprawdzenie kolizji z danym npc
+	for(int i = 0; i <npcs->size(); i++)
+	{
+		if(npcs->at(i)->player.GetCollisionPoints().intersects(playerCollisionRect))
+			return true;
+	}*/
+	
+	return false;
+}
+
+std::string Location::GetName()
+{
+	return locationName;
+}
+
+std::string Location::GetParent()
+{
+	return parentLocation;
+}
